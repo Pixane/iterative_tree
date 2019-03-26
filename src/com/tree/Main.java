bae4629e205cf04601e667645dcef9037b44c32f
Wojciech Jędras <wjedras92@gmail.com>
2019-03-26 16:36:41 +0100

    Basic sanity check.

--- src/com/tree/Main.java
+++ src/com/tree/Main.java
@@ -1,8 +1,23 @@
 package com.tree;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Node<Integer> root = new Node<>();
+        root.setData(1);
+        root.addChild(13);
+        root.addChild(192);
+        root.addChild(15);
+        root.getChildren().get(0).addChild(22);
+        root.getChildren().get(0).addChild(24);
+        root.getChildren().get(0).addChild(32);
+        root.getChildren().get(1).addChild(25);
+        root.getChildren().get(1).addChild(23);
+        root.getChildren().get(2).addChild(1254);
+        root.getChildren().get(2).addChild(125);
+        Node.DFS(root);
     }
 }
+
